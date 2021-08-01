46d40014d39ab79423c0945e3a96ebdb20f1350e
HASNAT JANI RAFIN <77789741+HasnatJaniRafin@users.noreply.github.com>
2021-08-01 12:44:43 +0800

    Create InputString.cpp

---
+++ InputString.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std ;
+int main ()
+{
+    char name [ 21 ] ;
+    cout << " Enter your full name :" ;
+    gets (name) ;
+
+    cout <<"welcome " <<name <<" to my website" <<endl ;
+    cout << " www.hasnatjanirafin.com" ;
+
+
+
+
+    return 0 ;
+
+}
